6a40b4599cec687f8fa24bd9e7a504048c8e2eba
midCenturyDude7 <mgriffes@gmail.com>
2023-01-10 22:28:37 -0800

    App setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-function App() {
+export default function App() {
 
   return (
-    <div>
-      Tenzies | Built with React
-    </div>
+    <main>
+      
+    </main>
   );
 }
-
-export default App;
