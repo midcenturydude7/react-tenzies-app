f270a268871543e2e18aa187fa8e5d5e162c9f0c
midCenturyDude7 <mgriffes@gmail.com>
2023-01-11 16:28:47 -0800

    Added 'Roll Dice' btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Die from "./Components/Die";
 
 export default function App() {
 
+  // eslint-disable-next-line no-unused-vars
+  const [dice, setDice] = React.useState(allNewDice());
+  
+  function allNewDice() {
+    const newDice = [];
+    for(let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  // eslint-disable-next-line react/jsx-key
+  const diceElements = dice.map(die => <Die value={die} />);
+
+
   return (
     <main>
-      
+      <div className="dice-container">
+        {diceElements}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   );
 }
